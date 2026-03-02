30b3ed9c06b71d37c1d375ae4ae8ea275623ea90
Bipin Kumar Sah <sahbipinkr2006@gmail.com>
2026-03-02 19:13:22 +0530

    Variables in JavaScript

---
+++ JavaScript/Variables.js
@@ -0,0 +1,30 @@
+let name = "Tony Stark";
+console.log(name);
+
+let age = 37;
+console.log(age);
+
+let x = null;
+console.log(x);
+
+let y = undefined;
+console.log(y);
+
+let boolx = true;
+console.log(boolx)
+
+let booly = false;
+console.log(booly)
+
+const pi = 3.14;
+//Constant values cannot be redefined.
+console.log(pi);
+
+{
+	let x = 5;
+	console.log(x);
+}
+{
+	let x = 10;
+	console.log(x);
+}
