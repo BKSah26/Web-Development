b293826ebbb34ea492f8463ff885042f11ce3af9
Bipin Kumar Sah <sahbipinkr2006@gmail.com>
2026-03-03 11:40:55 +0530

    Switch

---
+++ JavaScript/Switch.js
@@ -0,0 +1,17 @@
+let fruit = "Pineapple";
+switch(fruit){
+	case "Oranges":
+		console.log("Price: 50");
+		break;
+	case "Apples":
+		console.log("Price: 75");
+		break;
+	case "Bananas":
+		console.log("Price 40");
+		break;
+	case "Pineapple":
+		console.log("Price: 100");
+		break;
+	default:
+		console.log("Sorry, your fruit is not available.")
+}
