21162b0345a4fa16c70073a8cfda4edc16808c90
Bipin Kumar Sah <sahbipinkr2006@gmail.com>
2026-03-03 14:57:05 +0530

    Enhance Loops.js with additional loop examples
    Added examples for while, do-while, for-of, and for-in loops.

--- JavaScript/Loops.js
+++ JavaScript/Loops.js
@@ -1,5 +1,6 @@
+//For Loop
 for (let i = 1; i<=5; i++){
-	console.log(i);
+	console.log("i: ", i);
 }
 
 //Sum of first n numbers
@@ -9,3 +10,39 @@ for (let i=1; i<=n; i++){
 	sum+=i;
 }
 console.log("Sum: ", sum);
+
+//While Loop
+let i = 1;
+let mul = 5;
+while (i<=10){
+    console.log("5 x ", i, " = ", mul*i);
+    i++;
+}
+
+//Do While Loop
+i = 10;
+do{
+    console.log(i);
+    i++;
+}
+while(i<10);
+
+//For-Of Loop
+let size = 0;
+let name = "Maharana Pratap";
+for (let chr of name){
+    size++;
+}
+console.log("Size of String: ", size);
+
+//For-In Loop
+const cdt = {
+    name: "Ashmit Khare",
+    service: "Army",
+    course: 149,
+    cgpa: "7.5"
+}
+
+for (let key in cdt){
+    console.log(key, ": ", cdt[key]);
+}
