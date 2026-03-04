f7cff30b396e468e447abd80f754f399734fbe99
Bipin Kumar Sah <sahbipinkr2006@gmail.com>
2026-03-04 09:05:31 +0530

    Array Methods

--- JavaScript/Arrays.js
+++ JavaScript/Arrays.js
@@ -24,3 +24,43 @@ let cities = ["Delhi", "Kolkata", "Chennai", "Mumbai"];
 for (let ele of cities){
 	console.log(ele);
 }
+
+// Array Methods
+// Push and Pop
+brands = ["Casio", "Titan", "Timex"];
+console.log(brands);
+console.log(brands.length);
+brands.push("Sonata");
+console.log(brands);
+console.log(brands.length);
+
+let deletedBrand = brands.pop();
+console.log(deletedBrand);
+console.log(brands);
+console.log(brands.length);
+
+// To String
+let string = brands.toString();
+console.log(string);
+
+// Concatenation
+avengers = ["Iron Man", "Thor"];
+let xmen = ["Wolverine", "Magneto", "Nightcrawler", "Phoenix", "Cyclops"];
+console.log(avengers.concat(xmen));
+
+// Shift and Unshift
+avengers.unshift("Black Widow");
+console.log(avengers);
+let deletedAvenger = avengers.shift();
+console.log(deletedAvenger);
+console.log(avengers);
+
+// Slice
+console.log(xmen.slice(1, ));
+console.log(xmen.slice(2, 4));
+
+// Splice
+xmen.splice(3, 2, "Beast");
+console.log(xmen);
+xmen.splice(2, 0, "Quicksilver")
+console.log(xmen);
