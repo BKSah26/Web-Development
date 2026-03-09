c8fecf41adfc9cc55925813d9687d7ef94f6b328
Bipin Kumar Sah <sahbipinkr2006@gmail.com>
2026-03-09 19:47:41 +0530

    Document Object Model

---
+++ DocumentObjectModel/script.js
@@ -0,0 +1,16 @@
+let x = document.querySelector("h1");
+console.log(x);
+
+let y = document.querySelectorAll("h1");
+console.log(y);
+
+let z = document.querySelectorAll(".yes");
+console.log(z);
+
+let h2 = document.querySelector("h2");
+h2.style.color = "red";
+
+let h1 = document.querySelector("h1");
+h1.style.backgroundColor = "grey";
+
+h1.innerHTML = "NDA - Oscar Squadron(Abhimanyu Battalion)"
